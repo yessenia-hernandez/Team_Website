d48c15303fb6e6c9e854a20024a7424f0fce9c00
Yessenia  Hernandez <yh90926@srcs.loc>
2017-11-20 12:18:29 -0800

    days till next birthday code

--- functions.js
+++ functions.js
@@ -4,12 +4,14 @@
 
 
 
- //<script>
-               // var lastday = new Date("6/14/2018");
-               // var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
-              //  var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
-              //  var newhour = Math.floor(hours - (days * 24));
-             //   var minutes = (lastday.getTime() - today.getTime()) / 60000;
-             //   var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
-              //document.write(birthdayCountDown("12/15/2000")); // document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year");
-           //</script>
+ <script>
+                var lastday = new Date("02/02/2018");
+                var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
+                var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
+                var newhour = Math.floor(hours - (days * 24));
+                var minutes = (lastday.getTime() - today.getTime()) / 60000;
+                var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
+
+ document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year");
+              //document.write(birthdayCountDown("12/15/2000"));
+           </script>
