d22a8a47b9d1bf0f18c681d7302b571d02cd4cce
Yessenia  Hernandez <yh90926@srcs.loc>
2017-11-27 11:54:34 -0800

    deleting comments

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
                 var greeting = "Ciao!-Howdy!-Greetings!-Hola!-Bonjour!-Take me to your leader!"   var array = greeting.split("-");
                 var greeting = Math.random();
 
-<<<<<<< HEAD
 function
   var today = new Date();
   var date=  today.getDate
@@ -57,19 +56,14 @@ function
 function BirthdayCountDown(date){
                 var lastday = new Date(date);
                 var today = new Date();
->>>>>>> origin/master
                 var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
                 var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
                 var newhour = Math.floor(hours - (days * 24));
                 var minutes = (lastday.getTime() - today.getTime()) / 60000;
                 var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
 
-<<<<<<< HEAD
  document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year")
-=======
 
  return days + " days ";
 
 }
->>>>>>> origin/master
->>>>>>> origin/master
