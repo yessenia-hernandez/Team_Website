972dd5ade69188c94cf67d43463044f7396617b1
Kimberly C Reyes <kr61336@srcs.loc>
2017-11-27 12:19:35 -0800

    updates

--- functions.js
+++ functions.js
@@ -1,8 +1,9 @@
-                var greeting = "Ciao!-Howdy!-Greetings!-Hola!-Bonjour!-Take me to your leader!"   var array = greeting.split("-");
+                var greeting = "Ciao!-Howdy!-Greetings!-Hola!-Bonjour!-Take me to your leader!"
+                var array = greeting.split("-");
                 var greeting = Math.random();
 
-<<<<<<< HEAD
-function
+
+
   var today = new Date();
   var date=  today.getDate
   var year = today.getFullYear
@@ -52,39 +53,22 @@ function
 
 
   var lastday = new Date("02/02/2018");
-=======
-
-
-
-<<<<<<< HEAD
- //<script>
-               // var lastday = new Date("6/14/2018");
-               // var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
-              //  var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
-              //  var newhour = Math.floor(hours - (days * 24));
-             //   var minutes = (lastday.getTime() - today.getTime()) / 60000;
-             //   var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
-              //document.write(birthdayCountDown("12/15/2000")); // document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year");
-           //</script>
-=======
 
 
 function BirthdayCountDown(date){
                 var lastday = new Date(date);
-                var today = new Date();
->>>>>>> origin/master
+
+
                 var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
                 var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
                 var newhour = Math.floor(hours - (days * 24));
                 var minutes = (lastday.getTime() - today.getTime()) / 60000;
                 var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
 
-<<<<<<< HEAD
  document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year")
-=======
+
 
  return days + " days ";
 
 }
->>>>>>> origin/master
->>>>>>> origin/master
+
