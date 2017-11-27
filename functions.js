d9c1766f5a0a94b2824a546c9e373b0803303223
Kimberly C Reyes <kr61336@srcs.loc>
2017-11-27 11:50:09 -0800

    js update

--- functions.js
+++ functions.js
@@ -1,18 +1,15 @@
+                var greeting = "Ciao!-Howdy!-Greetings!-Hola!-Bonjour!-Take me to your leader!"   var array = greeting.split("-");
+                var greeting = Math.random();
 
 
 
 
-
-
-function BirthdayCountDown(date){
-                var lastday = new Date("02/02/2018");
-                var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
-                var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
-                var newhour = Math.floor(hours - (days * 24));
-                var minutes = (lastday.getTime() - today.getTime()) / 60000;
-                var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
-
-
- return " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year";
-              document.write(birthdayCountDown("12/15/2000"));
-}
+ //<script>
+               // var lastday = new Date("6/14/2018");
+               // var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
+              //  var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
+              //  var newhour = Math.floor(hours - (days * 24));
+             //   var minutes = (lastday.getTime() - today.getTime()) / 60000;
+             //   var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
+              //document.write(birthdayCountDown("12/15/2000")); // document.write("The are " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year");
+           //</script>
