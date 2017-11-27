f7a7514f3518b8935fb585d845471ee14d6b8052
Yessenia  Hernandez <yh90926@srcs.loc>
2017-11-27 11:47:18 -0800

    fixing js script

--- functions.js
+++ functions.js
@@ -5,7 +5,8 @@
 
 
 function BirthdayCountDown(date){
-                var lastday = new Date("02/02/2018");
+                var lastday = new Date(date);
+                var today = new Date();
                 var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
                 var hours = Math.ceil((lastday.getTime() - today.getTime()) / 3600000);
                 var newhour = Math.floor(hours - (days * 24));
@@ -13,6 +14,6 @@ function BirthdayCountDown(date){
                 var newminute = Math.ceil(minutes - (Math.floor((lastday.getTime() - today.getTime()) / 3600000) * 60));
 
 
- return " + days + " days, " + newhour + " hours, and " + newminute + " minutes untill the end of the school year";
-              document.write(birthdayCountDown("12/15/2000"));
+ return days + " days ";
+
 }
