64a39b4203f03d49c08ef1728bba1bbe1b96a399
Yessenia  Hernandez <yh90926@srcs.loc>
2017-11-29 11:07:07 -0800

    New functions2 file

---
+++ functions2.js
@@ -0,0 +1,17 @@
+function birthdayCountDown(date){
+                var lastday = new Date(date);
+                var today = new Date();
+                var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
+
+
+ return days + " days ";
+
+}
+
+function greetings(date) {
+
+}
+
+function dateMessage(date) {
+
+}
