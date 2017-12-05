9d55e1489203e9a0c8d813e571c2e03ac485a315
Yessenia  Hernandez <yh90926@srcs.loc>
2017-12-05 12:06:30 -0800

    birthday countdown js

--- functions2.js
+++ functions2.js
@@ -1,11 +1,14 @@
-function birthdayCountDown(date){
-                var lastday = new Date(date);
-                var today = new Date();
-                var days = Math.floor((lastday.getTime() - today.getTime()) / 86400000);
+function birthdayCountDown(date) {
+    var today = new Date();
+    var myBirthday = new Date(date);
+    myBirthday.setFullYear(today.getFullYear());
+    if (today.getTime() > myBirthday.getTime()) {
+        myBirthday.setFullYear(today.getFullYear() + 1);
+    }
+    var diff = myBirthday.getTime() - today.getTime();
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
 
-
- return days + " days ";
-
+    return ("My Birthday is in " + days+ " days :(");
 }
 
 function greetings(date) {
